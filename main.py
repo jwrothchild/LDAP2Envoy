b3c261972454aa188722b80f6253177b0711a6d4
jwrothchild <joanna@puppetlabs.com>
2015-03-09 16:48:21 -0700

    Return server response when posting to Envoy

--- main.py
+++ main.py
@@ -11,7 +11,12 @@ x.writeCSV()
 x.unbind()
 
 print("Posting CSV to Envoy...")
-url = 'https://signwithenvoy.com/api/configuration/employee_list?api_key=YOUR_API_KEY'
-files = {'employee_list.csv': open('employee_list.csv', 'rb')}
-r = requests.post(url, files=files)
-print("Post complete.")
+url = 'https://signwithenvoy.com/api/configuration/employee_list?api_key=YOUR_API_KEY'    
+files = {'file': open('employee_list.csv', 'rb')}
+try:
+    r = requests.post(url, files=files)
+    response = r.text
+    print(response)
+    print("Post complete.")
+except:
+    print("Post failed.")
